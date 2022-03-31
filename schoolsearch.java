e91daf29381c6a813f72f663ff3806f64ef639f3
Eric Lei <eric@MacBook-Pro-3.local>
2022-03-31 01:07:30 -0700

    added S[tudent]: <LastName> [B[us]]

--- schoolsearch.java
+++ schoolsearch.java
@@ -10,11 +10,55 @@ public class schoolsearch {
 
         ArrayList<Entry> entries = new ArrayList<Entry>();
         getStudentEntries(entries, studentsFile);
-        printAllEntries(entries);
+        //printAllEntries(entries);
 
+        getCommands(entries);
         
 
     }
+
+    public static void getCommands(ArrayList<Entry> entries){
+        Scanner command = new Scanner(System.in);
+        int quitFlag = 0;
+        while(quitFlag == 0){
+            String userCommand = command.nextLine();
+            String[] tokens = userCommand.split("\\s+");
+            //System.out.println(userCommand);
+            //System.out.println(tokens[0]);
+            if(tokens[0].equals("S:") || tokens[0].equals("Student:")){
+                if(tokens.length == 3){
+                    if(tokens[2].equals("B") || tokens[2].equals("Bus")){
+                        studentBus(entries, tokens[1]);
+                    }
+                }
+                if(tokens.length == 2){
+                    student(entries,tokens[1]);
+                }
+            }
+            if(tokens.length == 1){
+                if(tokens[0].equals("Q") || tokens[0].equals("Quit")){
+                    quitFlag = 1;
+                }
+            }
+        }
+    }
+
+    public static void studentBus(ArrayList<Entry> entries, String lastName){
+        for(int i = 0; i <entries.size(); i++){
+            if(entries.get(i).stLastName.equals(lastName)){
+                System.out.println(entries.get(i).stLastName+","+entries.get(i).stFirstName+","+entries.get(i).bus);
+            }
+        }
+    }
+    public static void student(ArrayList<Entry> entries, String lastName){
+        for(int i = 0; i <entries.size(); i++){
+            if(entries.get(i).stLastName.equals(lastName)){
+                System.out.println(entries.get(i).stLastName+","+entries.get(i).stFirstName+","+entries.get(i).grade+
+                ","+entries.get(i).classroom+","+entries.get(i).tLastName+","+entries.get(i).tFirstName);
+            }
+        }
+    }
+
     public static void getStudentEntries(ArrayList<Entry> entries, File studentsFile) throws FileNotFoundException{
         Scanner students = new Scanner(studentsFile);
         String studentLine = "";
@@ -22,12 +66,12 @@ public class schoolsearch {
             studentLine = students.nextLine();
             String[] tokens = studentLine.split(",");
             Entry studentEntry = new Entry();
-            studentEntry.stFirstName = tokens[0];
-            studentEntry.stLastName = tokens[1];
+            studentEntry.stFirstName = tokens[1];
+            studentEntry.stLastName = tokens[0];
             studentEntry.grade = Integer.parseInt(tokens[2]);
             studentEntry.classroom = Integer.parseInt(tokens[3]);
             studentEntry.bus = Integer.parseInt(tokens[4]);
-            studentEntry.gpa = tokens[5];
+            studentEntry.gpa = Double.parseDouble(tokens[5]);
             studentEntry.tLastName = tokens[6];
             studentEntry.tFirstName = tokens[7];
             entries.add(studentEntry);
